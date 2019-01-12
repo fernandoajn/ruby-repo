54dda0c8688cadd869813b5d42ddd1c96224fd0c
Fernando <me@ffaneto.com>
2019-01-11 23:15:13 -0300

    guessing game: functions

--- basics/guessing_game.rb
+++ basics/guessing_game.rb
@@ -1,35 +1,48 @@
-puts "Welcome to the Guessing Game!"
-puts "\n"
-puts "What is your name?"
-name = gets
-puts "\n"
+def welcome
+  puts "Welcome to the Guessing Game!"
+  puts "\n"
+  puts "What is your name?"
+  name = gets
+  puts "Prepare yourself, " + name
+end
 
-puts "We are going to start the game, " + name
-secret_number = 175
-puts "We selected a secret number. Why don't you try to guess what was it?"
-puts "\n\n"
+def random_secret_number
+  raffled = 175
+  puts "We selected a secret number. Why don't you try to guess what was it?"
+  puts "\n\n"
+  raffled
+end
 
-tries_limit = 3
-tentativa = 1
-
-for count in 1..tries_limit
+def ask_a_number(count, tries_limit)
   puts "Choose a number between " + count.to_s + " and " + tries_limit.to_s
   puts count.to_s + " attempt:"
   attempt = gets
+  attempt.to_i
+end
 
-  hit_number = attempt.to_i == secret_number
+def verify(attempt, secret_number)
+  hit_number = attempt == secret_number
 
   if hit_number
     puts "Gotcha!"
-    break
-  else 
-    higher = attempt.to_i < secret_number
+    return true
+  end
+
+    higher = attempt < secret_number
     if higher
       puts "Try a higher number"
     else 
       puts "Try a lower number"
     end
-  end
+    false
+end
 
-  puts "\n"
+welcome()
+secret_number = random_secret_number()
+
+tries_limit = 3
+
+for count in 1..tries_limit
+  attempt = ask_a_number(count, tries_limit)
+  break if verify(attempt, secret_number)
 end
