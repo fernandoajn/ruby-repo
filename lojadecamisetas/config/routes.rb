d3592c00dcca51e68d4bfb350564d090a04c1174
Fernando Augusto <fernandoajneto@gmail.com>
2019-04-21 14:06:06 -0300

    Resources

--- lojadecamisetas/config/routes.rb
+++ lojadecamisetas/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # get "[o nome que queremos para a URL]" => "[nome do Controller]#[nome da página]"
-  get "produtos" => "produtos#index"
-  get "produtos/new" => "produtos#new"
-  post "produtos" => "produtos#create"
+  # get "produtos" => "produtos#index"
+  # get "produtos/new" => "produtos#new"
+  # get "produtos/:id/remove" => "produtos#destroy", as: :produto
+  # post "produtos" => "produtos#create"
+  resources :produtos, only: [:new, :create, :destroy]
 
   get "usuarios" => "usuarios#index"
   root "produtos#index"
